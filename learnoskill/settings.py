4f96f895dcb4cd6d3409d329a5806593f9051608
mnath75 <mnath.snt@gmail.com>
2022-01-19 20:14:44 +0530

    message

--- learnoskill/settings.py
+++ learnoskill/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'learnoskill.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learnoskilldb',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
